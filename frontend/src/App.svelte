70d7c3f21e6dbf82b3d091f648dfebc951dfdcef
Björn Urban <burban@inovex.de>
2023-09-28 17:49:45 +0200

    feat(frontend): setup initial svelte frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { Router, Route, Link } from 'svelte-routing';
+  import routes from './routes.js';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<Router>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">KubeVoyage</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <Link class="nav-link" to="/">Login</Link>
+        </li>
+        <li class="nav-item active">
+          <Link class="nav-link" to="/register">Register</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/requests">Requests</Link>
+        </li>
+        <!-- Add more links as needed -->
+      </ul>
+    </div>
+  </nav>
+
+  {#each Object.entries(routes) as [path, Component]}
+    <Route path={path} let:params>
+      <Component {...params} />
+    </Route>
+  {/each}
+</Router>
+
+<style>
+  /* Add your styles here */
+</style>
