27e919e2b75b633370c41a2b121c6875009e9db6
Björn Urban <burban@inovex.de>
2023-10-11 21:16:53 +0200

    fix(frontend): set correct request url

--- frontend/src/Login.svelte
+++ frontend/src/Login.svelte
@@ -8,7 +8,7 @@
 
   async function login() {
     try {
-      const response = await fetch('http://localhost:8080/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
